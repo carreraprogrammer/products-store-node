9aef73275f6b691e74b057a2bcedd0515f6401a3
Daniel <carreraprogrammer@gmail.com>
2023-12-11 09:52:34 -0500

    Refactor file I/O to use async/await

--- models/product.js
+++ models/product.js
@@ -1,20 +1,15 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
-const p = path.join(
-  path.dirname(process.main.filename),
-  'data',
-  'products.json'
-);
+const p = path.join(__dirname, 'data', 'products.json');
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
-    }
-  });
+const getProductsFromFile = async () => {
+  try {
+    const fileContent = await fs.readFile(p, 'utf8');
+    return JSON.parse(fileContent);
+  } catch (err) {
+    return [];
+  }
 };
 
 module.exports = class Product {
@@ -25,16 +20,22 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {
-    getProductsFromFile(products => {
+  async save() {
+    try {
+      const products = await getProductsFromFile();
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
+      await fs.writeFile(p, JSON.stringify(products));
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
+  static async fetchAll() {
+    try {
+      return await getProductsFromFile();
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
   }
 };
