60f768969705d5a1294578061e250bcb3e266861
Daniel <carreraprogrammer@gmail.com>
2023-12-11 10:24:00 -0500

    Add product routes and functionality

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,25 @@
 const Product = require('../models/product');
 
+exports.getAddProduct = (req, res, next) => {
+  res.render('admin/add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect('/');
+};
+
 exports.getProducts = async (req, res, next) => {
   try {
     const products = await Product.fetchAll();
