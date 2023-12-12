837ba19585cb5086a13ba885983188695961e289
Daniel <carreraprogrammer@gmail.com>
2023-12-12 11:27:00 -0500

    Refactor Cart class and add getCart method

--- models/cart.js
+++ models/cart.js
@@ -58,7 +58,18 @@ module.exports = class Cart {
       fs.writeFile(p, JSON.stringify(updatedCart), err => {
         console.log(err);
       });
-         
+
     })
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+        cb(cart);
+      }
+    });
   }
 };
