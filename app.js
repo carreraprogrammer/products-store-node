14d05c9afa55ba88a5b0af2bed5d1dcadbcba06d
Daniel <carreraprogrammer@gmail.com>
2023-12-13 11:24:39 -0500

    Add database connection and query

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ const path = require('path');
 const express = require('express');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 const bodyParser = require('body-parser');
+
+db.execute('SELECT * FROM products')
+  .then(result => {
+    console.log(result[0], result[1]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
