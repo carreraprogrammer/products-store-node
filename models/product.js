97ff541f405dca71f1bd0aa8158b3b1bf1a1472c
Daniel <carreraprogrammer@gmail.com>
2023-12-13 16:40:38 -0500

    Refactor product model to use Sequelize

--- models/product.js
+++ models/product.js
@@ -1,37 +1,30 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING(255),
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING(255),
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING(255),
+    allowNull: false
   }
+});
 
-  async save() {
-    return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]);
-  }  
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static async findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-  }
-
-  static async deleteById(id) {
-
-    const products = await getProductsFromFile();
-
-    try {
-      const updatedProducts = products.filter(p => p.id !== id);
-      await fs.writeFile(p, JSON.stringify(updatedProducts));
-      console.log(`Product ${id} deleted`);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-};
+module.exports = Product;
