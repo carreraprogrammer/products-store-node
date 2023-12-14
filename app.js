02ec96ac5125b1f8a2c349953c97e6f7d241566b
Daniel <carreraprogrammer@gmail.com>
2023-12-13 21:26:47 -0500

    Add Cart and CartItem models

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -36,6 +38,10 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem});
+Product.belongsToMany(Cart, { through: CartItem});
 
 sequelize.sync().then(result => {
   User.findByPk(1)
